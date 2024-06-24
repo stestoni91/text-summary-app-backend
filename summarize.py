dd89a84ffc44c8c2ec7b567206165beea4042371
stestoni91 <125904012+stestoni91@users.noreply.github.com>
2024-06-24 11:59:53 +0000

    Use facebook/bart-large-cnn

--- summarize.py
+++ summarize.py
@@ -1,6 +1,6 @@
 from transformers import pipeline
 
-model = pipeline('summarization')
+model = pipeline('summarization', model='facebook/bart-large-cnn')
 
 def summarize(text):
     summary = model(text)[0]['summary_text']
